db0870537a435e94d4f8b48720ed932f6537e0ef
Artyom <OrtltrO@gmail.com>
2026-07-31 00:22:43 -0500

    feat: add GSE local source models

---
+++ source/Providers/GseLocal/GseLocalModels.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace PlayniteAchievements.Providers.GseLocal
+{
+    internal sealed class GseLocalSourceLocation
+    {
+        public string AppId { get; set; }
+        public string InstallDirectory { get; set; }
+        public string SettingsDirectory { get; set; }
+        public string SchemaPath { get; set; }
+        public string RuntimeDirectory { get; set; }
+        public string RuntimePath { get; set; }
+        public bool RuntimeDirectoryExists { get; set; }
+        public bool RuntimeStateExists { get; set; }
+    }
+
+    internal sealed class GseLocalSnapshot
+    {
+        public string AppId { get; set; }
+        public string SchemaPath { get; set; }
+        public string RuntimePath { get; set; }
+        public DateTime GeneratedAtUtc { get; set; }
+        public bool StateKnown { get; set; }
+        public bool IsCompleteSnapshot { get; set; }
+        public List<GseLocalAchievement> Achievements { get; set; } = new List<GseLocalAchievement>();
+        public List<string> Diagnostics { get; set; } = new List<string>();
+
+        public bool IsAuthoritative => StateKnown && IsCompleteSnapshot;
+    }
+
+    internal sealed class GseLocalAchievement
+    {
+        public string AchievementId { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public string LockedIconPath { get; set; }
+        public string UnlockedIconPath { get; set; }
+        public bool IsHidden { get; set; }
+        public bool IsUnlocked { get; set; }
+        public DateTime? UnlockTimeUtc { get; set; }
+    }
+}
